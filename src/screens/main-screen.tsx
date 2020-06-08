e8ebf6a18280cc959113857b42e40496f7697a1b
rafiandria23 <rafiandria23@gmail.com>
2020-06-08 22:02:50 +0700

    - Enabled bracket spacing in Prettier config. - Installed react-native-config for loading env vars from .env files.

---
+++ src/screens/main-screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export interface IMainScreenProps {}
+
+export default function MainScreen(props: IMainScreenProps) {
+  return (
+    <View>
+
+    </View>
+  );
+}
